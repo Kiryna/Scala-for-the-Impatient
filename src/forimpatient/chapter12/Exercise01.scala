692b3cd29a7bb0c4f6dad2bb2e74ffc767df3bad
Iryna Kharaborkina <ija.khb@gmail.com>
2016-07-24 19:26:10 +0300

    Higher-Order Functions solution 01

---
+++ src/forimpatient/chapter12/Exercise01.scala
@@ -0,0 +1,21 @@
+package forimpatient.chapter12
+
+/**
+  * Created by Iryna Kharaborkina on 7/24/16.
+  *
+  * Solution to the Chapter 12 Exercise 01 'Scala for the Impatient' by Horstmann C.S.
+  *
+  * Write a function values(fun: (Int) => Int, low: Int, high: Int) that yields a collection of function
+  * inputs and outputs in a given range. For example, values(x => x * x, -5, 5) should produce a
+  * collection of pairs (-5, 25), (-4, 16), (-3, 9), . . ., (5, 25).
+  */
+object Exercise01 extends App {
+  override def main(args: Array[String]): Unit = {
+    println("Chapter 12 Exercise 01")
+
+    def values(fun: (Int) => Int, low: Int, high: Int) = for (el <- low to high) yield (el, fun(el))
+
+    println(values(x => x * x, -5, 5))
+    println(values(x => -x, -5, 5))
+  }
+}
