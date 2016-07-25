40b2571fdcd4c6a4e392a01b1c30b5866b4e5e3f
Iryna Kharaborkina <ija.khb@gmail.com>
2016-07-25 11:18:25 +0300

    Added exercise 10 solution

---
+++ src/forimpatient/chapter01/Exercise10.scala
@@ -0,0 +1,22 @@
+package forimpatient.chapter01
+
+/**
+  * Created by Iryna Kharaborkina on 7/25/16. 
+  *
+  * Solution to the Chapter 01 Exercise 10 'Scala for the Impatient' by Horstmann C.S.
+  *
+  * What do the take, drop, takeRight, and dropRight string functions do? What advantage or
+  * disadvantage do they have over using substring?
+  */
+object Exercise10 extends App {
+  override def main(args: Array[String]): Unit = {
+    println("Chapter 01 Exercise 10")
+
+    val str = "Exercise09"
+
+    println(str.take(3))        // returns string, containing first n symbols; here n == 3
+    println(str.drop(3))        // returns string, containing all but first n symbols; here n == 3
+    println(str.dropRight(3))   // returns string, containing all but last n symbols; here n == 3
+    println(str.takeRight(3))   // returns string, containing last n symbols; here n == 3
+  }
+}
