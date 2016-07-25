9acca94e274acb64f72b21ce4a77bbbaf5f6a026
Iryna Kharaborkina <ija.khb@gmail.com>
2016-07-25 10:51:50 +0300

    Added exercise 8 solution

---
+++ src/forimpatient/chapter01/Exercise08.scala
@@ -0,0 +1,22 @@
+package forimpatient.chapter01
+
+import scala.BigInt._
+import scala.util.Random
+
+/**
+  * Created by Iryna Kharaborkina on 7/25/16.
+  *
+  * Solution to the Chapter 01 Exercise 08 'Scala for the Impatient' by Horstmann C.S.
+  *
+  * One way to create random file or directory names is to produce a random BigInt and convert it
+  * to base 36, yielding a string such as "qsnvbevtomcj38o06kul". Poke around Scaladoc to find a way of
+  * doing this in Scala.
+  */
+object Exercise08 extends App {
+  override def main(args: Array[String]): Unit = {
+    println("Chapter 01 Exercise 08")
+
+    val name = probablePrime(100, Random).toString(36)
+    println(name)
+  }
+}
